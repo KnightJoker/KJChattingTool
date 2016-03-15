da3bf900463c4285237e4223269823d5e69ddeba
“KnightJoker” <cbearxl@gmail.com>
2016-03-15 23:33:06 +0800

    add model

--- ChattingTool/Class/Model/Message.h
+++ ChattingTool/Class/Model/Message.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    MessageTypeSelf,
+    MessageTypeOther
+}MessageType;
+
 @interface Message : NSObject
+
+@property (nonatomic,copy) NSString *text;
+@property (nonatomic,copy) NSString *time;
+@property (nonatomic,assign) MessageType type;
+@property (nonatomic,assign,getter = isHiddemTime) BOOL hiddemTime;
+- (instancetype)initWithDic:(NSDictionary *)dic;
++ (instancetype)messageWithDic:(NSDictionary *)dic;
++ (NSMutableArray *)messageList;
 
 @end
