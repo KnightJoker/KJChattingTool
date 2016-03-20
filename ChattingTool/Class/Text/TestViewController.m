9aa6a6945ffe4ebbda6377277a77b26068eed412
“KnightJoker” <cbearxl@gmail.com>
2016-03-20 15:50:53 +0800

    test

---
+++ ChattingTool/Class/Text/TestViewController.m
@@ -0,0 +1,13 @@
+//
+//  TestViewController.m
+//  ChattingTool
+//
+//  Created by 来自遥远星系的核心巡洋舰 on 16/3/20.
+//  Copyright © 2016年 KnightJoker. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@implementation TestViewController
+
+@end
